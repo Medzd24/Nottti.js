5895f193b79f9a2d2b5a027c247afb131072986b
Medzd24 <150199255+Medzd24@users.noreply.github.com>
2023-11-13 17:39:46 +0100

    Create Notti.js

---
+++ Notti.js
@@ -0,0 +1,7 @@
+<div class="important-topics-container">
+  <span class="important-topics"><b>مواضيع هامة&nbsp;</b></span>
+  <a class="subscribe-btn">
+    <span class="badge">4</span>
+    <span class="bell-emoji">🔔</span>
+  </a>
+</div>
